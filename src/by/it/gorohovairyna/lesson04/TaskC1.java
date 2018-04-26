7eef49d51254ef49eb84df9749433b251f7ff735
Gorohova <3053193@gmail.com>
2018-04-26 08:21:23 +0300

    lesson04 c

--- src/by/it/gorohovairyna/lesson04/TaskC1.java
+++ src/by/it/gorohovairyna/lesson04/TaskC1.java
@@ -43,7 +43,44 @@ public class TaskC1 {
         Scanner scanner = new Scanner(System.in);
         int s = scanner.nextInt();
 
-        if (s < 300 | s > 3000)
+        if (s < 300 || s > 3000)
             System.out.println("Мы вам перезвоним!");
+        else
+            for (int month = 0; month <=14; month++){
+            double pay=0;
+            if (month>0 && month<13)
+                pay=s*1.5;
+            if (month>5 && month<9)
+                pay=s;
+            switch (month){
+                case 1:
+                    System.out.println("За январь начислено $"+pay); break;
+                case 2:
+                    System.out.println("За февраль начислено $"+pay); break;
+                case 3:
+                    System.out.println("За март начислено $"+pay); break;
+                case 4:
+                    System.out.println("За апрель начислено $"+pay); break;
+                case 5:
+                    System.out.println("За май начислено $"+pay); break;
+                case 6:
+                    System.out.println("За июнь начислено $"+pay); break;
+                case 7:
+                    System.out.println("За июль начислено $"+pay); break;
+                case 8:
+                    System.out.println("За август начислено $"+pay); break;
+                case 9:
+                    System.out.println("За сентябрь начислено $"+pay); break;
+                case 10:
+                    System.out.println("За октябрь начислено $"+pay); break;
+                case 11:
+                    System.out.println("За ноябрь начислено $"+pay); break;
+                case 12:
+                    System.out.println("За декабрь начислено $"+pay); break;
+                default:
+                    System.out.println("За месяц"+month+"начислено $"+pay); break;
+            }
+            if (pay == 666)break;
+            }
     }
 }
